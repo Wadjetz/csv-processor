adaf97d966be07f2010767af2b905be9bbb21209
Egor Berezovskiy <egor.neon@gmail.com>
2018-04-13 10:18:07 +0200

    add scopt

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.12.5"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.5.11"
 libraryDependencies += "org.iq80.leveldb" % "leveldb" % "0.9"
+libraryDependencies += "com.github.scopt" %% "scopt" % "3.7.0"
---
+++ src/main/scala/Cli.scala
@@ -0,0 +1,31 @@
+import java.io.File
+
+case class Cli (
+  input: File = new File("./input.csv"),
+  aggratings: File = new File(".aggratings.csv"),
+  lookupuser: File = new File(".lookupuser.csv"),
+  lookupProduct: File = new File(".lookup_product.csv"),
+)
+
+object Cli {
+  val parser = new scopt.OptionParser[Cli]("csv_processor") {
+    head("csv_processor", "0.0.1")
+
+    opt[File]('i', "input").required().valueName("<file>").
+      action((x, c) => c.copy(input = x)).
+      text("input is a required file property")
+
+    opt[File]('a', "aggratings").valueName("<file>").
+      action((x, c) => c.copy(aggratings = x)).
+      text("aggratings is a file property")
+
+    opt[File]('u', "lookupuser").valueName("<file>").
+      action((x, c) => c.copy(lookupuser = x)).
+      text("lookupuser is a file property")
+
+    opt[File]('p', "lookupProduct").valueName("<file>").
+      action((x, c) => c.copy(lookupProduct = x)).
+      text("lookupProduct is a file property")
+  }
+}
+
